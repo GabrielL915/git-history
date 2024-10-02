e37ddcccb70734a2b5eece43d2a6316b99f1aca6
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-10-02 15:01:01 -0300

    feat: add data-source

---
+++ packages/github-api/data-source/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/github-history.data-source';
---
+++ packages/github-api/data-source/src/lib/github-history.data-source.ts
@@ -0,0 +1,5 @@
+export class GithubHistoryDataSource  {
+  constructor() {
+    console.log('GithubHistoryDataSource constructor');
+  }
+}
